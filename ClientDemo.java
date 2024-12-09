4b8ed384fa6804b6ca4b473a427cf8e07af6b86d
SathwikaPotti <2200030579@kluniversity.in>
2024-12-09 21:45:32 +0530

    Create ClientDemo.java

---
+++ ClientDemo.java
@@ -0,0 +1,32 @@
+package com.klef.jfsdlab;
+
+import jakarta.websocket.Session;
+
+public class ClientDemo {
+	public static void main(String[] args) {
+       
+		  private static void insert{
+		        Customer customer1 = new Customer();
+		        customer1.setName("Alice");
+		        customer1.setEmail("alice@example.com");
+		        customer1.setAge(28);
+		        customer1.setLocation("New York");
+
+		        Customer customer2 = new Customer();
+		        customer2.setName("Bob");
+		        customer2.setEmail("bob@example.com");
+		        customer2.setAge(22);
+		        customer2.setLocation("Los Angeles");
+
+		        Customer customer3 = new Customer();
+		        customer3.setName("Charlie");
+		        customer3.setEmail("charlie@example.com");
+		        customer3.setAge(35);
+		        customer3.setLocation("Chicago");
+
+		        
+		    }
+		
+
+}
+}
